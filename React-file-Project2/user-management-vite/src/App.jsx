47dab87d62a77a2db37e7f5ebbb074b0024e55d1
bhuwanthapa45 <hellobhuwanthapa45@gmail.com>
2025-11-27 19:31:09 +0530

    feature: added react router dom

--- React-file-Project2/user-management-vite/src/App.jsx
+++ React-file-Project2/user-management-vite/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import UserForm from './components/UserForm';
 import UserTable from './components/userTable';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Table from './pages/Table';
+
+
+
 
 
 
@@ -9,6 +15,11 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50 p-4 md:p-8 font-sans">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/table" element={<Table />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <div className="max-w-7xl mx-auto">
         <header className="mb-8">
           <h1 className="text-3xl font-bold text-gray-900">User Management</h1>
